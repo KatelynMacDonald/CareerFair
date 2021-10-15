3f5d2dab42fc1428dde3e808e4054d06fc1dfde1
loganhue52 <logan.fehn@evsck12.com>
2021-10-15 14:07:38 -0500

    military, filter, js file

--- get.js
+++ get.js
@@ -9,45 +9,145 @@ const firebaseConfig = {
     appId: "1:513234627322:web:bb29fecd5ceeea674b3743",
     measurementId: "G-1GKEYRGJXZ",
     databaseURL: "https://sictc-career-fair-default-rtdb.firebaseio.com/"
-  };
+    };
 
 firebase.initializeApp(firebaseConfig);
 const app = initializeApp(firebaseConfig);
-
 var database = firebase.database();
-//vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-var listy = new Array();
-var objList = new Array();
 
-function getVals(database){
+//https://stackoverflow.com/questions/610406/javascript-equivalent-to-printf-string-format
+//modifying String's prototype in order to add python-like formatting - i love stack overflow
+//literally making a function for the String class
+if (!String.prototype.format) {
+    String.prototype.format = function() {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function(match, number) { 
+            return typeof args[number] != 'undefined'
+                ? args[number]
+                : match
+            ;
+        });
+    };
+}
+
+//four functions for filtering
+//script will run addAll() by default but when a filter button is click it resets the body and uses an if statement to only pass in colleges,companies,etc to addCard()
+function addAll(){
     database.ref("Items").on('value', (snapshot) => {
+        // this resets the body since the .on() will run everytime the database is updated and cause duplication
+        document.getElementById("body-output").innerHTML = "";
         const data = snapshot.val();
         var items = Object.values(data);
-        console.log(items);
-        listy.push(Object.keys(items).map((key) => [items[key]]));
-        for(let k in listy){
-            for (let j in listy[k]){
-                for (let l in listy[k][j]){
-                    objList.push(Object.values(listy[k][j][l]));
-                }
-            }
+        for (let l in items){
+            addCard(items[l]);
         }
-        //setUpTable(objList)
     });
 }
 
-getVals(database);
-
-function setUpTable(l){
-    var table = "<table> <tr><th>ID #</th><th>Description</th><th>Location</th><th>Qty</th><th>Tag</th></tr>";
-    for (var rowIndex=0; rowIndex < l.length; rowIndex++) {
-        var row = "<tr><td>#" + rowIndex + "</td>";
-        for(var colIndex = 0; colIndex < 4; colIndex++) {
-            var x = l[rowIndex][colIndex];
-            row += "<td>" + x + "</td>";
+function addColleges(){
+    database.ref("Items").on('value', (snapshot) => {
+        document.getElementById("body-output").innerHTML = "";
+        const data = snapshot.val();
+        var items = Object.values(data);
+        for (let l in items){
+            if (items[l]["type"]==="College"){
+                addCard(items[l]);
+            }
         }
-        table += row + "</tr>";
+    });
+}
+
+function addCompanies(){
+    database.ref("Items").on('value', (snapshot) => {
+        document.getElementById("body-output").innerHTML = "";
+        const data = snapshot.val();
+        var items = Object.values(data);
+        for (let l in items){
+            if (items[l]["type"]==="Company"){
+                addCard(items[l]);
+            }
+        }
+    });
+}
+
+function addMilitary(){
+    database.ref("Items").on('value', (snapshot) => {
+        document.getElementById("body-output").innerHTML = "";
+        const data = snapshot.val();
+        var items = Object.values(data);
+        for (let l in items){
+            if (items[l]["type"]==="Military"){
+                addCard(items[l]);
+            }
+        }
+    });
+}
+
+addAll();
+
+//https://www.w3schools.com/jsref/event_onclick.asp
+document.getElementById("allbutton").onclick = function() {addAll()};
+document.getElementById("collegesbutton").onclick = function() {addColleges()};
+document.getElementById("companiesbutton").onclick = function() {addCompanies()};
+document.getElementById("militarybutton").onclick = function() {addMilitary()};
+
+function addCard(l){
+
+    var prevDiv = document.getElementById("body-output").innerHTML;
+
+    var div = `
+    <div class="card mb-3" style="max-width: 75%;" align="left">
+        <div class="row g-0">
+            <div class="col-md-2">
+                <img src="{0}" class="img-fluid rounded-start" alt="{1} logo" >
+            </div>
+            <div class="col-md-7">
+                <div class="card-body">
+                    <h5 class="card-title" style="font-size: 27px;">{1}</h5>
+                    <div class="card-body">`.format(l["logo"],l["name"]);
+
+    if (l["type"]==="College"){
+        div = div + `<a href="{0}" class="btn btn-danger">{1} Website</a>`.format(l["web"],l["name"]);
+    } else if (l["type"]==="Company"){
+        div = div + `<a href="{0}" class="btn btn-primary">{1} Website</a>`.format(l["web"],l["name"]);
+    } else {
+        div = div + `<a href="{0}" class="btn btn-success">{1} Website</a>`.format(l["web"],l["name"]);
     }
-    //console.log(table);
-    document.getElementById("output").innerHTML = table + "</table>"; 
+        
+    div = div + `   </div>
+                    <p class="card-text">{0}</p>
+                </div>
+            </div>
+            <div class="col-md-3" align="center" style="padding-right:15px;padding-left:15px;">
+                <div class="card-body">
+                    <p class="card-text">Interest Areas:</p>
+                </div> 
+                <ul class="list-group list-group-flush">`.format(l["desc"]);
+    //adding to div depending on how many interest areas are blank
+    //will make this smaller is a final version
+    if (l["ia2"]==="" && l["ia3"]==="" && l["ia4"]==="" && l["ia5"]===""){
+        div = div +`<li class="list-group-item">{0}</li>`.format(l["ia1"]);
+    } else if (l["ia3"]==="" && l["ia4"]==="" && l["ia5"]===""){
+        div = div +`<li class="list-group-item">{0}</li> 
+                    <li class="list-group-item">{1}</li>`.format(l["ia1"],l["ia2"]);
+    } else if (l["ia4"]==="" && l["ia5"]===""){
+        div = div +`<li class="list-group-item">{0}</li> 
+                    <li class="list-group-item">{1}</li> 
+                    <li class="list-group-item">{2}</li>`.format(l["ia1"],l["ia2"],l["ia3"]);
+    } else if (l["ia5"]===""){
+        div = div +`<li class="list-group-item">{0}</li> 
+                    <li class="list-group-item">{1}</li> 
+                    <li class="list-group-item">{2}</li> 
+                    <li class="list-group-item">{3}</li>`.format(l["ia1"],l["ia2"],l["ia3"],l["ia4"]);
+    } else {
+        div = div +`<li class="list-group-item">{0}</li> 
+                    <li class="list-group-item">{1}</li> 
+                    <li class="list-group-item">{2}</li> 
+                    <li class="list-group-item">{3}</li> 
+                    <li class="list-group-item">{4}</li>`.format(l["ia1"],l["ia2"],l["ia3"],l["ia4"],l["ia5"]);
+    }
+
+    div = div + `</ul></div></div></div>`;
+
+    document.getElementById("body-output").innerHTML = prevDiv + div;
 }
